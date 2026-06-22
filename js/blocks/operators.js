9296d1f8ce693a3e5878b7c5160ab4e3614b6f21
JuniorSierraMorales <125913687+juniorsierra18@users.noreply.github.com>
2026-06-22 18:18:56 -0500

    se actualizo

--- js/blocks/operators.js
+++ js/blocks/operators.js
@@ -1,134 +1,154 @@
 Blockly.defineBlocksWithJsonArray([
 
-{
-    "type":"math_operation",
+    // 1. Operaciones Matemáticas (+, -, *, /, %)
+    {
+        "type": "math_operation",
+        "message0": "%1 %2 %3",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "LEFT"
+            },
+            {
+                "type": "field_dropdown",
+                "name": "OP",
+                "options": [
+                    ["+", "+"],
+                    ["-", "-"],
+                    ["*", "*"],
+                    ["/", "/"],
+                    ["%", "%"]
+                ]
+            },
+            {
+                "type": "input_value",
+                "name": "RIGHT"
+            }
+        ],
+        "output": "Number",
+        "colour": 230
+    },
 
-    "message0":"%1 %2 %3",
+    // 2. Operadores de Comparación (==, !=, >, <, >=, <=)
+    {
+        "type": "comparison",
+        "message0": "%1 %2 %3",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "LEFT"
+            },
+            {
+                "type": "field_dropdown",
+                "name": "OP",
+                "options": [
+                    ["==", "=="],
+                    ["!=", "!="],
+                    [">", ">"],
+                    ["<", "<"],
+                    [">=", ">="],
+                    ["<=", "<="]
+                ]
+            },
+            {
+                "type": "input_value",
+                "name": "RIGHT"
+            }
+        ],
+        "output": "Boolean",
+        "colour": 65
+    },
 
-    "args0":[
+    // 3. Operadores Lógicos (&&, ||)
+    {
+        "type": "logic_operator",
+        "message0": "%1 %2 %3",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "LEFT"
+            },
+            {
+                "type": "field_dropdown",
+                "name": "OP",
+                "options": [
+                    ["&&", "&&"],
+                    ["||", "||"]
+                ]
+            },
+            {
+                "type": "input_value",
+                "name": "RIGHT"
+            }
+        ],
+        "output": "Boolean",
+        "colour": 65
+    },
 
-        {
-            "type":"input_value",
-            "name":"LEFT"
-        },
+    // 4. Valor Numérico Simple
+    {
+        "type": "number_value",
+        "message0": "%1",
+        "args0": [
+            {
+                "type": "field_number",
+                "name": "VALUE",
+                "value": 0
+            }
+        ],
+        "output": "Number",
+        "colour": 180
+    },
 
-        {
-            "type":"field_dropdown",
-            "name":"OP",
+    // 5. NUEVO: Crear variable y asignarle una operación/valor
+    {
+        "type": "create_variable_with_op",
+        "message0": "%1 %2 = %3",
+        "args0": [
+            {
+                "type": "field_dropdown",
+                "name": "TYPE",
+                "options": [
+                    ["int", "int"],
+                    ["float", "float"],
+                    ["double", "double"],
+                    ["string", "string"],
+                    ["bool", "bool"]
+                ]
+            },
+            {
+                "type": "field_input",
+                "name": "VAR_NAME",
+                "text": "nuevaVariable"
+            },
+            {
+                "type": "input_value",
+                "name": "VALUE"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 330
+    },
 
-            "options":[
-
-                ["+","+"],
-                ["-","-"],
-                ["*","*"],
-                ["/","/"],
-                ["%","%"]
-            ]
-        },
-
-        {
-            "type":"input_value",
-            "name":"RIGHT"
-        }
-    ],
-
-    "output":"Number",
-
-    "colour":230
-},
-
-{
-    "type":"comparison",
-
-    "message0":"%1 %2 %3",
-
-    "args0":[
-
-        {
-            "type":"input_value",
-            "name":"LEFT"
-        },
-
-        {
-            "type":"field_dropdown",
-            "name":"OP",
-
-            "options":[
-
-                ["==","=="],
-                ["!=","!="],
-                [">",">"],
-                ["<","<"],
-                [">=",">="],
-                ["<=","<="]
-            ]
-        },
-
-        {
-            "type":"input_value",
-            "name":"RIGHT"
-        }
-    ],
-
-    "output":"Boolean",
-
-    "colour":65
-},
-
-{
-    "type":"logic_operator",
-
-    "message0":"%1 %2 %3",
-
-    "args0":[
-
-        {
-            "type":"input_value",
-            "name":"LEFT"
-        },
-
-        {
-            "type":"field_dropdown",
-            "name":"OP",
-
-            "options":[
-
-                ["&&","&&"],
-                ["||","||"]
-            ]
-        },
-
-        {
-            "type":"input_value",
-            "name":"RIGHT"
-        }
-    ],
-
-    "output":"Boolean",
-
-    "colour":65
-}
-
-]);
-Blockly.defineBlocksWithJsonArray([
-
-{
-    "type":"number_value",
-
-    "message0":"%1",
-
-    "args0":[
-
-        {
-            "type":"field_number",
-            "name":"VALUE",
-            "value":0
-        }
-    ],
-
-    "output":"Number",
-
-    "colour":180
-}
+    // 6. NUEVO: Asignar una operación/valor a una variable ya existente
+    {
+        "type": "assign_to_variable",
+        "message0": "%1 = %2",
+        "args0": [
+            {
+                "type": "field_input",
+                "name": "VAR_NAME",
+                "text": "variableExistente"
+            },
+            {
+                "type": "input_value",
+                "name": "VALUE"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 330
+    }
 
 ]);
